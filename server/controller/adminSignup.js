b68b5c048788624f28a9d6a961bab664e8e1a77a
rks9800 <rs994797@gmail.com>
2020-08-05 12:24:59 +0530

    admin signup fix

--- server/controller/adminSignup.js
+++ server/controller/adminSignup.js
@@ -1,67 +1,58 @@
 //express
-const express = require('express');
+const express = require("express");
 //express router
 const router = express.Router();
 //mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //Nodemailer
 const nodemailer = require("nodemailer");
 //Bcrypt
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 //Validator
-const validator = require('validator');
+const validator = require("validator");
 
 //Configuration files
-const bcr = require("../config/bcrypt")
-const admindetail = require("../config/adminDetail")
+const bcr = require("../config/bcrypt");
+const admindetail = require("../config/adminDetail");
 
 //Database Table Models
-const admin = require("../model/admin")
+const admin = require("../model/admin");
 
 //#region Admin Request Hande Route
-exports.admin_request_gamo = async(req, res) =>{
-    const Gmail = req.query.Id
-    const Password = req.query.Password
-    const Secretkey = req.query.SecretKey
-    if(!Gmail || !Password || !Secretkey){
+exports.admin_request_gamo = async (req, res) => {
+  const Gmail = req.query.Id;
+  const Password = req.query.Password;
+  const Secretkey = req.query.SecretKey;
+  if (!Gmail || !Password || !Secretkey) {
+    return res.status(400).json({ err: "No Credential Entered" });
+  }
+  if (Password !== admindetail.Password) {
+    return res.status(400).json({ err: "Password Wrong" });
+  }
+  if (Gmail !== admindetail.Gmail) {
+    return res.status(400).json({ err: "Id Wrong" });
+  }
+  if (Secretkey !== admindetail.Secretkey) {
+    return res.status(400).json({ err: "Secret Key Wrong" });
+  }
+  await admin.create(
+    {
+      Email: Gmail,
+      Password: bcrypt.hashSync(Password, bcr.round),
+      Status: "A",
+    },
+    async (err, result) => {
+      if (err) {
+        console.log(err);
         return res
-        .status(400)
-        .json({err: "No Credential Entered"})
+          .status(400)
+          .json({ err: "Some Error Occured Please Try Again" });
+      }
+      if (result === null) {
+        return res.status(400).json({ err: "Cound Not Create Admin Access" });
+      } else {
+        return res.status(400).json({ msg: "Your Admin Access Created" });
+      }
     }
-    if(Password !== admindetail.Password){
-        return res
-        .status(400)
-        .json({err: "Password Wrong"})
-    }
-    if(Gmail !== admindetail.Id){
-        return res
-        .status(400)
-        .json({err: "Id Wrong"})
-    }
-    if(Secretkey !== admindetail.Secretkey){
-        return res
-        .status(400)
-        .json({err: "Secret Key Wrong"})
-    }
-    await admin.create({
-        Email: Gmail,
-        Password: bcrypt.hashSync(Password, bcr.round),
-        Status: "A",
-    }, async(err, res) =>{
-        if(err){
-            console.log(err)
-            return res
-            .status(400)
-            .json({err: "Some Error Occured Please Try Again"})
-        }
-        if(res === null){
-            return res
-            .status(400)
-            .json({err: "Cound Not Create Admin Access"})
-        }else{
-            return res
-            .status(400)
-            .json({msg: "Your Admin Access Created"})
-        }
-    })
-}
+  );
+};
--- server/model/admin.js
+++ server/model/admin.js
@@ -1,38 +1,37 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const AdminSchema = new Schema({
-    Email: {
-        type: String,
-        unique: true,
-    },
-    Phone: {
-        type: String,
-        unique: true,
-    },
-    Password: {
-        type: String,
-        maxlength: 6,
-        default: null,
-    },
-    Role: {
-        type: String,
-        required: true,
-        default: "SuperAdmin",
-    },
-    Profile: {
-        type: String,
-        default: null,
-    },
-    Name: {
-        type: String,
-        required: true,
-        default: "SuperAdmin"
-    },
-    Status: {
-        type: String,
-        required: true,
-        default: null,
-    }
-})
-module.exports = mongoose.model("admin", AdminSchema)
+  Email: {
+    type: String,
+    unique: true,
+  },
+  Phone: {
+    type: String,
+    unique: true,
+  },
+  Password: {
+    type: String,
+    default: null,
+  },
+  Role: {
+    type: String,
+    required: true,
+    default: "SuperAdmin",
+  },
+  Profile: {
+    type: String,
+    default: null,
+  },
+  Name: {
+    type: String,
+    required: true,
+    default: "SuperAdmin",
+  },
+  Status: {
+    type: String,
+    required: true,
+    default: null,
+  },
+});
+module.exports = mongoose.model("admin", AdminSchema);
